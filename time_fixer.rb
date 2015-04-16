f2f1dc14bd43b354710d55faebbeac3020e69353
Joseph <heron.joseph@gmail.com>
2015-04-16 14:47:56 -0400

    ADDED: time fixing to run script

--- time_fixer.rb
+++ time_fixer.rb
@@ -1,21 +1,37 @@
 
-# TODO change so that it can also take stdin and fix the values.
-file_name = nil
-if ARGV.size == 1
-    file_name = ARGV[0]
-else 
-    Kernel.exit(true)
-end
-
-file = File.new("#{file_name}.fixed", "w+")
-
-IO.foreach(file_name) do |line|
-
+# Parse the time string
+def handleLine(io, line)
     # Scan the line and check if it is the real value
     result = line.scan(/real[\s\t]+([0-9]+)m([0-9]+\.[0-9]+)s/)
     if result && result[0]
         # total it up
         time = result[0][0].to_i * 60 + result[0][1].to_f
-        file.puts time
+        io.puts time
+    end
+end
+
+file_name = nil
+from_stdin = false
+if ARGV.size == 1
+    file_name = ARGV[0]
+else
+    from_stdin = true
+#    Kernel.exit(true)
+end
+
+
+# Handle the type of input
+if !from_stdin
+    file = File.new("#{file_name}.fixed", "w+")
+    IO.foreach(file_name) do |line|
+        handleLine(file, line)
+    end
+    file.close()
+else
+    # Read in all of the lines.
+    while !$stdin.eof?
+        line = gets
+        #puts "FIXER"
+        handleLine($stdout, line)
     end
 end
